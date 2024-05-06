0fd8b3cde4cfbe1fbb7cf2e455f3fc797cb204ea
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-05-06 22:54:25 +0300

    Add even game logic

---
+++ src/even.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+import hello from './cli.js';
+import getRandomInt from './getRandomInt.js';
+import isEven from './isEven.js';
+
+const even = () => {
+  const name = hello();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let correctAnswersLeft = 3;
+
+  while (correctAnswersLeft > 0) {
+    const randomInt = getRandomInt();
+    console.log(`Question: ${randomInt}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    const correctAnswer = isEven(randomInt) ? 'yes' : 'no';
+
+    if (correctAnswer !== answer.toLocaleLowerCase()) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}`);
+      return;
+    }
+
+    console.log('Correct!');
+    correctAnswersLeft -= 1;
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default even;
