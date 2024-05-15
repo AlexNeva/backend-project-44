989d59e44a64b79205887e16055182d61748552e
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-05-15 10:46:45 +0300

    Remove isEven module

--- src/games/even.js
+++ src/games/even.js
@@ -1,9 +1,12 @@
 import app from '../index.js';
 import getRandomInt from '../getRandomInt.js';
-import isEven from '../isEven.js';
 
 const purpose = 'Answer "yes" if the number is even, otherwise answer "no".';
+
 const getNum = () => getRandomInt(1, 100);
+
+const isEven = (num) => num % 2 === 0;
+
 const getResult = (num) => (isEven(num) ? 'yes' : 'no');
 
 export default () => app(purpose, getNum, getResult);
