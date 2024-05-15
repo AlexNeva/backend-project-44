d4844ca83ac8fe5448d4243034e5230c9b0cd8eb
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-05-15 10:47:29 +0300

    Remove isPrime module

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,6 +6,20 @@ const purpose = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const getNum = () => getRandomInt(1, 100);
 
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 const getResult = (num) => (isPrime(num) ? 'yes' : 'no');
 
 export default () => app(purpose, getNum, getResult);
