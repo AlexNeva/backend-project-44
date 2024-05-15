ba8004fc712d7a0509d21cee3dc0cb1cef65cce1
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-05-15 10:48:54 +0300

    Remove getProgression module

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,6 +2,18 @@ import getProgression from '../getProgression.js';
 import getRandomInt from '../getRandomInt.js';
 import app from '../index.js';
 
+const getProgression = (length = 10, step = 2, startNum = 1) => {
+  const progression = [startNum];
+
+  for (let i = 1; i < length; i += 1) {
+    const newNum = progression[i - 1] + step;
+
+    progression.push(newNum);
+  }
+
+  return progression;
+};
+
 const purpose = 'What number is missing in the progression?';
 
 const getQuestion = () => {
